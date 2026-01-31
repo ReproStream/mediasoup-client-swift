ae06b5e83a6b47e7683ec95f6666b92c5de5209e
joemorsman <joe@widebeamdigital.com>
2026-01-31 23:50:35 +0000

    Fix CFBundleExecutable in framework Info.plist files

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let version = "0.9.0-renamed"
 let mediasoupChecksum = "9a7b41413826ac9488980a2b0adb6f99abb78505579bde2f972fae73623d8234"
-let webrtcChecksum = "83f9119ce78cc72725f49e500d06beda3211b8e1c9d18f4f46c90b6c99bf750f"
+let webrtcChecksum = "c1327dafae9e2feff083219446322dab4451029ce3ecdaa5e8bd3c0ed640ac45"
 
 let package = Package(
     name: "Mediasoup-Client-Swift",
@@ -24,12 +24,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mediasoup",
-            url: "https://github.com/ReproStream/mediasoup-client-swift/releases/download/0.9.0-renamed/Mediasoup.xcframework.zip",
+            url: "https://github.com/YOUR-ORG/mediasoup-client-swift/releases/download/0.9.0-renamed/Mediasoup.xcframework.zip",
             checksum: mediasoupChecksum
         ),
         .binaryTarget(
             name: "MediasoupWebRTC",
-            url: "https://github.com/ReproStream/mediasoup-client-swift/releases/download/0.9.0-renamed/MediasoupWebRTC.xcframework.zip",
+            url: "https://github.com/YOUR-ORG/mediasoup-client-swift/releases/download/0.9.0-renamed/MediasoupWebRTC.xcframework.zip",
             checksum: webrtcChecksum
         )
     ]
