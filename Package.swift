266d6c63dabdfd0e0e9616438075cfd1f9263599
joemorsman <joe@widebeamdigital.com>
2026-01-31 23:17:14 +0000

    Update Package.swift with renamed MediasoupWebRTC framework and ReproStream URLs

--- Package.swift
+++ Package.swift
@@ -1,38 +1,36 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-
-let version = "0.9.0"
-let mediasoupChecksum = "ce70d0d16499a9972c0b627589946a18859552a6de09b97bb507f898e50504a9"
-let webrtcChecksum = "d6f5f751ca3aad5ba9e74896ba6e5a4869b3a50ac6def2a069357277d94ea99a"
-
+let version = "0.9.0-renamed"
+let mediasoupChecksum = "9a7b41413826ac9488980a2b0adb6f99abb78505579bde2f972fae73623d8234"
+let webrtcChecksum = "26f0439dcd94aa6dd4f64ff761803c855411773c91bec061b3734cd317777a90"
 
 let package = Package(
-	name: "Mediasoup-Client-Swift",
-	platforms: [
-		.iOS(.v14)
-	],
-	products: [
-		.library(
-			name: "Mediasoup",
-			targets: [
-				"Mediasoup",
-				"WebRTC"
-			]
-		)
-	],
-	dependencies: [
-	],
-	targets: [
-		.binaryTarget(
-			name: "Mediasoup",
-			url: "https://github.com/VLprojects/mediasoup-client-swift/releases/download/\(version)/Mediasoup.xcframework.zip",
-			checksum: mediasoupChecksum
-		),
-		.binaryTarget(
-			name: "WebRTC",
-			url: "https://github.com/VLprojects/mediasoup-client-swift/releases/download/\(version)/WebRTC.xcframework.zip",
-			checksum: webrtcChecksum
-		)
-	]
+    name: "Mediasoup-Client-Swift",
+    platforms: [
+        .iOS(.v14),
+        .tvOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "Mediasoup",
+            targets: [
+                "Mediasoup",
+                "MediasoupWebRTC"
+            ]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "Mediasoup",
+            url: "https://github.com/ReproStream/mediasoup-client-swift/releases/download/0.9.0-renamed/Mediasoup.xcframework.zip",
+            checksum: mediasoupChecksum
+        ),
+        .binaryTarget(
+            name: "MediasoupWebRTC",
+            url: "https://github.com/ReproStream/mediasoup-client-swift/releases/download/0.9.0-renamed/MediasoupWebRTC.xcframework.zip",
+            checksum: webrtcChecksum
+        )
+    ]
 )
