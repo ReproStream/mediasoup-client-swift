5335446b16968598aa000ab9b611bd42bad44279
joemorsman <joe@widebeamdigital.com>
2026-02-01 00:14:08 +0000

    Fix Mediasoup Swift interfaces: Replace 'import WebRTC' with 'import MediasoupWebRTC'

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let version = "0.9.0-renamed"
-let mediasoupChecksum = "9a7b41413826ac9488980a2b0adb6f99abb78505579bde2f972fae73623d8234"
+let mediasoupChecksum = "ded433d6d29825b25b063b9fcf05cd4eb7bab3091e12e2db4731daff75d9eb44"
 let webrtcChecksum = "4d2521902c0108aa58861cb5db4182f0772914b9e014c8a61f3f6089f7a1a8a3"
 
 let package = Package(
@@ -24,12 +24,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mediasoup",
-            url: "https://github.com/ReproStream/mediasoup-client-swift/releases/download/0.9.0-renamed/Mediasoup.xcframework.zip",
+            url: "https://github.com/YOUR-ORG/mediasoup-client-swift/releases/download/0.9.0-renamed/Mediasoup.xcframework.zip",
             checksum: mediasoupChecksum
         ),
         .binaryTarget(
             name: "MediasoupWebRTC",
-            url: "https://github.com/ReproStream/mediasoup-client-swift/releases/download/0.9.0-renamed/MediasoupWebRTC.xcframework.zip",
+            url: "https://github.com/YOUR-ORG/mediasoup-client-swift/releases/download/0.9.0-renamed/MediasoupWebRTC.xcframework.zip",
             checksum: webrtcChecksum
         )
     ]
