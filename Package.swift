7272d13f79863ed27ded2b2282cbc3a31291c9ec
joemorsman <joe@widebeamdigital.com>
2026-02-01 09:26:16 +0000

    fix: update MediasoupWebRTC install_name ID to reference itself correctly
    Fixed framework ID from @rpath/WebRTC.framework/WebRTC to
    @rpath/MediasoupWebRTC.framework/MediasoupWebRTC
    
    New checksum: 0d8957dc2c16312bf052d0db33f4f3d8ae220611ec22f4681f06eaaeb91681d6

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let version = "0.9.0-renamed"
 let mediasoupChecksum = "2a4cc0cb5bb2b3e63b0d8651b87a38631bdf29700f1929c0b8bd6d8b3594bcff"
-let webrtcChecksum = "e41c957d0d7134930b83174e8e912cc2af996613e0061c0400a2840af7e8e4e4"
+let webrtcChecksum = "0d8957dc2c16312bf052d0db33f4f3d8ae220611ec22f4681f06eaaeb91681d6"
 
 let package = Package(
     name: "Mediasoup-Client-Swift",
